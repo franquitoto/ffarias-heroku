2da548f2daad89dce484b99f2600097d11f0c3d1
franquitoto <francosamuel1999@gmail.com>
2022-08-04 14:50:45 -0300

    comiteamos

--- src/services/config/index.js
+++ src/services/config/index.js
@@ -1,11 +1,10 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 dotenv.config();
 
 export default {
-    MONGO_ATLAS_URL: process.env.MONGO_ATLAS_SRV || 'mongoSRV',
-    GMAIL_EMAIL: process.env.GMAIL_EMAIL || 'email@gmail.com',
-  GMAIL_PASSWORD: process.env.GMAIL_PASSWORD || 'password',
-  GMAIL_NAME: process.env.GMAIL_NAME || 'GMAIL owner name',
-}
- 
+  MONGO_ATLAS_URL: process.env.MONGO_ATLAS_SRV || "mongoSRV",
+  GMAIL_EMAIL: process.env.GMAIL_EMAIL || "email@gmail.com",
+  GMAIL_PASSWORD: process.env.GMAIL_PASSWORD || "password",
+  GMAIL_NAME: process.env.GMAIL_NAME || "GMAIL owner name",
+};
